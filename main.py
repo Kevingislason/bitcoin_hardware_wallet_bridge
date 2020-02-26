6a91aba400a8daec5385fc0496d17bcef00780a4
Kevin Gislason <kevingislason@gmail.com>
2020-02-25 23:08:32 -0500

    create password form working

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from os import path
+from functools import partial
 import sys
 import configparser
-from PyQt5.QtWidgets import QWidget, QApplication, QGridLayout, QPushButton, QLineEdit
-from PyQt5.QtCore import Qt
+import bcrypt
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
 
 
 class App(QWidget):
@@ -38,20 +40,60 @@ class App(QWidget):
 
         configfile.close()
 
+    # todo: move this into its own class extending layout
     def show_first_login_view(self):
         self.layout = QGridLayout()
-        SET_PASSWORD_BUTTON_WIDTH = 115
-        set_password_button = QPushButton()
-        set_password_button.setFixedWidth(SET_PASSWORD_BUTTON_WIDTH)
-        set_password_button.setText('Save password')
-        passwordForm = QLineEdit()
-        passwordForm.setEchoMode(QLineEdit.Password)
-        self.layout.addWidget(passwordForm, 0, 0, Qt.AlignCenter)
-        self.layout.addWidget(set_password_button, 1, 0, Qt.AlignCenter)
+        create_password_instruction_label = QLabel()
+        CREATE_PASSWORD_INSTRUCTION_LABEL_TEXT = 'Choose a password with:\n• at least eight characters\n• at least one capital letter\n• at least one number'
+        create_password_instruction_label.setText(
+            CREATE_PASSWORD_INSTRUCTION_LABEL_TEXT)
+
+        create_password_form = QLineEdit()
+        create_password_form.setEchoMode(QLineEdit.Password)
+        CREATE_PASSWORD_BUTTON_WIDTH = 115
+        create_password_button = QPushButton()
+        create_password_button.setFixedWidth(CREATE_PASSWORD_BUTTON_WIDTH)
+        create_password_button.setText('Save password')
+        create_password_button.clicked.connect(
+            partial(self.handle_click_create_password_button, create_password_form, create_password_instruction_label))
+
+        self.layout.addWidget(
+            create_password_instruction_label, 0, 0, Qt.AlignCenter)
+        self.layout.addWidget(create_password_form, 1, 0, Qt.AlignCenter)
+        self.layout.addWidget(create_password_button, 2, 0, Qt.AlignCenter)
         self.setLayout(self.layout)
 
-    # def validate_password(self):
-    #
+    def handle_click_create_password_button(self, password_form, create_password_instruction_label):
+        password_validation_errors = self.validate_password(
+            password_form.text())
+        if len(password_validation_errors):
+            create_password_instruction_label_text = "•" + '\n•'.join(
+                password_validation_errors)
+            create_password_instruction_label.setText(
+                create_password_instruction_label_text)
+        else:
+            self.save_password(password_form.text())
+
+    def validate_password(self, password):
+        validation_errors = []
+        if len(password) < 8:
+            validation_errors.append(
+                "password must have at least eight characters")
+        if not any(c.isupper() for c in password):
+            validation_errors.append(
+                "password must have at least one uppercase letter")
+        if not any(c.isdigit() for c in password):
+            validation_errors.append(
+                "password must have at least one number")
+        return validation_errors
+
+    def save_password(self, password):
+        password = password.encode('utf-8')
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(password, salt)
+        with open('secrets.bfe', 'w+') as secrets:
+            secrets.write(hashed_password.decode('utf-8'))
+        secrets.close()
 
 
 def main():
